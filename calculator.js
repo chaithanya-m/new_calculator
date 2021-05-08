6f5a29a396876c6eaed303f5fa85ef1bdd31cc9d
chaithanya <chaithanyamadduru03@gmail.com>
2021-05-08 10:00:49 +0530

    complet code added

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function div(valu){
+	document.getElementById("text_bar").value += valu;
+}
+function equal(){
+	var value1= document.getElementById("text_bar").value;
+			let first_value= eval(value1);
+			document.getElementById("text_bar").value=first_value;	
+}
+function remove(){
+	document.getElementById("text_bar").value="";
+}
+function backspace() {
+    
+    var value1 = document.getElementById("text_bar").value;
+    document.getElementById("text_bar").value=value1.substring(0,value1.length -1);
+}
+function squar(){
+	var value1 = document.getElementById("text_bar").value;
+	document.getElementById("text_bar").value=value1*value1;
+}
